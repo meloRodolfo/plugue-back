1b223ffcc044f73a0998d9675119774c4643284a
Milena Veríssimo <milenaverissimo@id.uff.br>
2021-12-11 10:25:51 -0300

    adicionando comentarios

--- models/comment.js
+++ models/comment.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
   };
   comment.init({
     userId: DataTypes.STRING,
-    IdeaId: DataTypes.STRING,
+    ideaId: DataTypes.STRING,
     text: DataTypes.STRING
   }, {
     sequelize,
